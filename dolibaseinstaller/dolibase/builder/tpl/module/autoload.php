624bb5811b16e6f6fa61917d9d03f0c2eb1283d0
AXeL-dev <contact.axel.dev@gmail.com>
2020-07-03 12:32:37 +0100

    Upgrade dolibase version

--- dolibaseinstaller/dolibase/builder/tpl/module/autoload.php
+++ dolibaseinstaller/dolibase/builder/tpl/module/autoload.php
@@ -1,11 +1,15 @@
 <?php
 
+global $dolibase_config;
+
+$__DIR__ = dirname(__FILE__);
+
 // Load module configuration (mandatory)
-require __DIR__ . '/config.php';
+$dolibase_config = @include($__DIR__ . '/config.php');
 
 // Load Dolibarr environment (mandatory)
-if (false === (@include_once __DIR__ . '/../main.inc.php')) { // From htdocs directory
-	require_once __DIR__ . '/../../main.inc.php'; // From "custom" directory
+if (false === (@include_once $__DIR__ . '/../main.inc.php')) { // From htdocs directory
+	require_once $__DIR__ . '/../../main.inc.php'; // From "custom" directory
 }
 
 // Load Dolibase
